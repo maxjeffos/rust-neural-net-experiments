9a2c25a9240022d9835d02c2a11817dea81ad1bd
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-11-21 08:20:57 -0500

    cleanup simple linear regression

--- test3-simple-linear-regression/src/main.rs
+++ test3-simple-linear-regression/src/main.rs
@@ -1,7 +1,7 @@
 use common::point::Point;
 
 fn main() {
-    let the_data = vec![
+    let data = vec![
         Point::new(0.0, 0.0),
         Point::new(2.0, 2.0),
         Point::new(4.0, 4.0),
@@ -10,23 +10,33 @@ fn main() {
         Point::new(5.0, 5.0),
     ];
 
-    the_data.iter().for_each(|p| {
+    data.iter().for_each(|p| {
         println!("{:?}", p);
     });
     println!();
 
     let theta_init = (1.0, 0.0); // ideal line for this data is (0.0, 1.0)
+    let epocs = 200;
 
+    let theta_final = linear_regression(&data, theta_init, epocs);
+
+    println!("theta0: {}", theta_final.0);
+    println!("theta1: {}\n", theta_final.1);
+
+    println!("final cost: {}", cost(theta_final.0, theta_final.1, &data));
+}
+
+fn linear_regression(data: &[Point], theta_init: (f64, f64), epocs: usize) -> (f64, f64) {
     let mut theta0 = theta_init.0;
     let mut theta1 = theta_init.1;
 
+    // extract learning_rate to a parameter
     let learning_rate = 0.1;
 
-    let max_iterations = 200;
     let mut iteration = 0;
 
     loop {
-        let cost_fn_partial_derivatives = compute_partial_derivatives(theta0, theta1, &the_data);
+        let cost_fn_partial_derivatives = compute_partial_derivatives(theta0, theta1, &data);
         let cost_fn_pd_0 = cost_fn_partial_derivatives.0;
         let cost_fn_pd_1 = cost_fn_partial_derivatives.1;
 
@@ -35,16 +45,13 @@ fn main() {
 
         iteration += 1;
 
-        if iteration >= max_iterations {
+        if iteration >= epocs {
             println!("max iterations reached: {}", iteration);
             break;
         }
     }
 
-    println!("theta0: {}", theta0);
-    println!("theta1: {}", theta1);
-
-    println!("final cost: {}", cost(theta0, theta1, &the_data));
+    (theta0, theta1)
 }
 
 fn compute_partial_derivatives(theta0: f64, theta1: f64, data: &[Point]) -> (f64, f64) {
@@ -77,4 +84,31 @@ fn cost(theta0: f64, theta1: f64, data: &[Point]) -> f64 {
         cost += diff_squared;
     }
     cost / (2.0 * data.len() as f64)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use common::point::Point;
+
+    #[test]
+    fn it_yields_the_correct_result() {
+        let data = vec![
+            Point::new(0.0, 0.0),
+            Point::new(2.0, 2.0),
+            Point::new(4.0, 4.0),
+            Point::new(6.0, 6.0),
+            Point::new(3.0, 3.0),
+            Point::new(5.0, 5.0),
+        ];
+
+        let theta_init = (1.0, 0.0); // ideal line for this data is (0.0, 1.0)
+
+        let epocs = 200;
+
+        let theta_final = linear_regression(&data, theta_init, epocs);
+
+        assert_eq!(theta_final.0, 0.007864997397684767);
+        assert_eq!(theta_final.1, 0.9982229772190276);
+    }
 }
